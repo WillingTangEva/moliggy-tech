d9a66bbc6114b2d5ac384b4ed2b951dd0c99d734
WillingTangEva <132994531+WillingTangEva@users.noreply.github.com>
2025-04-21 15:55:31 +0800

    uu

--- src/app/components/Solutions.tsx
+++ src/app/components/Solutions.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import { motion } from 'framer-motion';
 import { Card, CardContent } from './ui/card';
 import { cn } from '../utils/cn';
