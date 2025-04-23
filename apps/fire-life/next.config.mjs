dd532bc14a45b84792c2d0309651256ea0db90b7
WillingTangEva <132994531+WillingTangEva@users.noreply.github.com>
2025-04-23 17:40:14 +0800

    hello

--- apps/fire-life/next.config.mjs
+++ apps/fire-life/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: false,
-    domains: ['vercel.com'],
-  },
-  output: 'standalone',
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
