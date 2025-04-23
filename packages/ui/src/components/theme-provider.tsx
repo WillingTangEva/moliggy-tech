b169ff8bc7bc5b6b4de6f43aaf7488351b36530d
WillingTangEva <132994531+WillingTangEva@users.noreply.github.com>
2025-04-23 12:47:25 +0800

    up

--- packages/ui/src/components/theme-provider.tsx
+++ packages/ui/src/components/theme-provider.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { ReactNode } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
